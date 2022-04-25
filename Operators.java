f9d81fab9826af13790ffe5cb13f3a1ea410be70
jaojaii <70026678+jaojaii@users.noreply.github.com>
2022-04-26 00:15:18 +0530

    new javaFiles

---
+++ Operators.java
@@ -0,0 +1,12 @@
+class Operators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
